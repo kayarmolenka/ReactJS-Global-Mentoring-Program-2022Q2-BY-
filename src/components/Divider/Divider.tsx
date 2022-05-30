b2ec75e8e90c14bac0cf7244d31e63e3da978be5
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2022-05-30 10:03:58 +0300

    task_3 fix small issue

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import styles from './Divider.module.scss';
 
 export const Divider = () => {
-  return <div className={styles.divider}></div>;
+  return <div className={styles.divider} />;
 };
